2c876ff0a54e7dbdb6bcfdb8696c0bb0ab0f8fac
Tushar <tushar.agarwal.3762@gmailcom>
2021-08-28 22:40:14 +0530

    This Application is used to fetch the Gst details of a customer

--- app/src/main/java/com/example/gst/View/MainActivity.java
+++ app/src/main/java/com/example/gst/View/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
